46f3afcd8c593585985ffeb2ac7f6801580c98b0
MatiasMtz <@4442@holbertonschool.com>
2022-02-04 08:20:04 -0800

    single digit numbers of base 10 with putchar.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Responsible for the execution and termination of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+for (l = 0; l <= 9; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
