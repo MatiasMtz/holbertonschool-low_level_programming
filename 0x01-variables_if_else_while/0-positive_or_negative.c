829b446fa9cf896ffcf421350752579e9afd2b80
MatiasMtz <@4442@holbertonschool.com>
2022-02-04 07:13:41 -0800

    n is positive, negative or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-  printf("is positive %d\n", n);
+printf(n, "is positive %d\n");
 }
 else if (n == 0)
 {
-  printf("is zero %d\n", n);
+printf(n, "is zero %d\n");
 }
 else
 {
-  printf("is negative %d\n", n);
+printf(n, "is negative %d\n");
 }
 return (0);
 }
