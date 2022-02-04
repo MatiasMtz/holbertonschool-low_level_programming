c744994693da5741a97cc42fd03eb1705feed59f
MatiasMtz <@4442@holbertonschool.com>
2022-02-04 05:42:17 -0800

    a-z with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Responsible for the execution and termination of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+while (n < 123)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
