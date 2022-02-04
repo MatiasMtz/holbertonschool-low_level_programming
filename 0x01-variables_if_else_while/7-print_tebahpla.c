231b18374844ad9c5501b330c995fcaa02d69a46
MatiasMtz <@4442@holbertonschool.com>
2022-02-04 08:26:16 -0800

    reverse alphabet with putchar.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Responsible for the execution and termination of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 122;
+while (n > 96)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
