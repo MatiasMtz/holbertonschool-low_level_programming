929bbae0391ee46f2cec4692bd081cfa450e80b1
MatiasMtz <@4442@holbertonschool.com>
2022-02-04 08:07:38 -0800

    Single digit base 10.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Responsible for the execution and termination of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+for (l = 0; l <= 9; l++)
+{
+printf("%d", l);
+}
+return (0);
+}
