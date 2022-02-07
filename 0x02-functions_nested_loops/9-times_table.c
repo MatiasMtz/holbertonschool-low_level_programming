1b5506ec74c8aa59c3c4a39341ff13e68d26a26b
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 14:06:55 -0800

    prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
- * times_table - asda
+ * times_table - prints 9 time tables.
  *
  */
 void times_table(void)
 {
-	int i;
-	int a;
-	int res;
-	for (i = 0 ; i <= 9 ; i++)
-	{
-		for (a = 0 ; a <= 9 ; a++)
-		{
-			res = i * a;
-			if (res >= 10 && a != 0)
-			{
-				_putchar(res / 10 + '0');
-			}
-			else if (res < 10 && a != 0)
-			{
-				_putchar(' ');
-			}
-			_putchar(res % 10 + '0');
-			if (a != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+int i;
+int a;
+int res;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (a = 0 ; a <= 9 ; a++)
+{
+res = i * a;
+if (res >= 10 && a != 0)
+{
+_putchar(res / 10 + '0');
+}
+else if (res < 10 && a != 0)
+{
+_putchar(' ');
+}
+_putchar(res % 10 + '0');
+if (a != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
