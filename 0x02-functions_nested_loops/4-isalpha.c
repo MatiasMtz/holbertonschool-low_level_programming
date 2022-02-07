c991ce9bc70c82a78b502acaa6bb55fffb7dc7f7
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 10:06:37 -0800

    checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isalpha; - Print something if there is a letter.
+ * _isalpha - Print something if there is a letter.
  * @c: Variable.
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
 {
 
-if (c > 97 && c < 122 && c > 65 && c < 90)
+if ((c > 97 && c < 122) || (c > 65 && c < 90))
 {
 return (1);
 }
