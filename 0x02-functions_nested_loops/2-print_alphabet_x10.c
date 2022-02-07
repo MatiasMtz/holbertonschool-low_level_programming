71a13e91517a5ea6ab1b2a57beef375e141be2d6
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 09:07:32 -0800

    Alphabet 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point.
+ * Putchar - writes a character to stdout.
+ * Return: 0 if success.
+ */
+void print_alphabet_x10(void)
+{
+int a;
+char l;
+for (a = 0; a < 10; a++)
+{
+for (a = 'a' ; a <= 'z' ; a++)
+_putchar(a);
+}
+_putchar('\n');
+}
+}
