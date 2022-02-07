a2ccdcc7a18ff10d91019796d5a3512bfa529b79
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 08:54:53 -0800

    Printing _putchar with putchar.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	int i;
-	char string[] = "_putchar";
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(string[i]);
-	}
-	_putchar('\n');
-	return (0);
+int i;
+char string[] = "_putchar";
+for (i = 0; i < 8; i++)
+{
+_putchar(string[i]);
+}
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints letters to the output.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+	while (a < 123)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
