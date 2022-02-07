958791758196886b2a342cdce240d1bbf55bb8c8
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 04:09:41 -0800

    printing _putchar with putchar.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#define  LENGTH 80
 /**
- * main - Prints out a string to stdout.
+ * main - Entry point.
+ * Putchar - writes a character to stdout.
  * Return: 0 if success.
  */
 int main(void)
 {
-	printf("_putchar,\n");
+	int i, ch;
+	char buffer[LENGTH + 1] = "_putchar\n";
+
 	return (0);
 }
