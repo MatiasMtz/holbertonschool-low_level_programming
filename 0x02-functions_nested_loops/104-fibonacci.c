621308a8bb864956f2552be8b1cab555fb16fb61
MatiasMtz <@4442@holbertonschool.com>
2022-02-07 16:00:21 -0800

    finds and prints the first 98 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
+
 /**
- *main - print first 98 fibonacci numbers.
- *Return: 0 success.
- */
+*main - printing the first 98 Fibonacci numbers starting with 1 and 2
+*
+*Return: return 0 if success.
+*/
+
 int main(void)
 {
-	long n1 = 1;
-	long n2 = 0;
-	long sum;
-	int count;
+long int i, f, s, n, h2, h1, h3, h4, h0, h5;
 
-	for (count = 0; count <= 98; count++)
-	{
-		sum = n1 + n2;
-		n2 = n1;
-		n1 = sum;
-		if (count != 98)
-		{
-			printf("%ld, ", sum);
-		}
-		else
-		{
-			printf("%ld\n", sum);
-		}
-	}
-	return (0);
+f = 0;
+s = 1;
+i = 0;
+while (i < 91)
+{
+n = f + s;
+f = s;
+s = n;
+printf("%ld", n);
+printf(",");
+printf(" ");
+i++;
+}
+
+h4 = f / 1000000000;
+h3 = f % 1000000000;
+h2 = s / 1000000000;
+h1 = s % 1000000000;
+while (i < 97)
+{
+h0 = h4 + h2;
+h5 = h3 + h1;
+printf("%ld%ld", h0, h5);
+if (i != 96)
+{
+printf(",");
+printf(" ");
+}
+h4 = h2;
+h3 = h1;
+h2 = h0;
+h1 = h5;
+i++;
+}
+printf("\n");
+return (0);
 }
