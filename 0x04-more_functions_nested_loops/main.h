4e714a937dd3023ccf9a04771e2f736e5c3bc8f8
MatiasMtz <4442@holbertonschool.com>
2022-02-09 09:06:36 -0800

    main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,8 @@ int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
