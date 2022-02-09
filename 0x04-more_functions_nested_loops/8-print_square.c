e783ee588570e97b5b576b6a425771a4990eecaf
MatiasMtz <4442@holbertonschool.com>
2022-02-09 11:02:36 -0800

    squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - check the code
+ * @size: variable.
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+		for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y <= size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
