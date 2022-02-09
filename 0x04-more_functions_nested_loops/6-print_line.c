482b4fb668b12e506614d2d39f913c372d9953b5
MatiasMtz <4442@holbertonschool.com>
2022-02-09 09:50:16 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,11 @@
  */
 void print_line(int n)
 {
-	int i = 0;
+	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
