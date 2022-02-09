2a0f91cb1c830d230ace9f8943fd7e096b2580f2
MatiasMtz <4442@holbertonschool.com>
2022-02-09 06:48:11 -0800

    prints the numbers with putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - check the code.
+ * 
+ */
+void print_numbers(void)
+{
+int numbers;
+for (numbers >= 0 ; numbers <= 0 ; numbers++)
+{
+_putchar(numbers);
+}
+_putchar ('\n');
+}
