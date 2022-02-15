00c1a0f87d2b4f8c946521f4d8d8631fc6f76922
MatiasMtz <4442@holbertonschool.com>
2022-02-15 09:11:42 -0800

    compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * *_strcmp - check the code
+ * @s1;  Pointer.
+ * @s2: pointer.
+ * Return: success or fail.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int n = 0;
+	int x = 0;
+
+	while (s1[n] != '\0')
+	{
+		n++;
+	}
+	while (s2 [x] != '\0')
+	{
+		x++;
+	}
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	if (n > x)
+	{
+		return (s1[x]);
+	}
+	else if (x < n)
+	{
+		return (s2[n] * -1);
+	}
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ int _strlen(char *s);
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
