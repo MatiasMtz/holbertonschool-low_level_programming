fb60732e03bd27ed7d4b158abae79cc750dd6df0
MatiasMtz <4442@holbertonschool.com>
2022-02-15 12:44:13 -0800

    encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,29 @@
 #include "main.h"
 /**
- * string_toupper - check the code
+ * *leet - check the code
  * @s:  Pointer to a
- * Return: return 0 if success.
+ * Return: return s.
  */
 char *leet(char *s)
 {
 	int i = 0;
+	int n = 0;
 
-	
+	char let[] = "AaEeOoTtLl";
+	char num[] = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		n = 0;
+		while (let[n] != '\0')
+		{
+			if (s[i] == let[n])
+			{
+				s[i] = num[n];
+			}
+			n++;
+		}
+		i++;
+	}
+	return (s);
+}
