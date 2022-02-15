3139541cfa55d3a363eb7473c78002a6c7c63ff7
MatiasMtz <4442@holbertonschool.com>
2022-02-15 12:11:03 -0800

    capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *cap_string - check the code
+ * @s: First variable.
+ * Return: Retorno
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+				s[i] = s[i] - 32;
+		continue;
+		}
+	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
+	    s[i] == ':' || s[i] == '.' || s[i] == '?' || s[i] == '!' ||
+	    s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+	    s[i] == '}')
+	{
+		if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] = s[i + 1] - 32;
+		}
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * string_toupper - check the code
+ * @s:  Pointer to a
+ * Return: return 0 if success.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+
+	
