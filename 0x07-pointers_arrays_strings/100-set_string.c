3a8bcdf2bc86e59166d65a13c656895850ff03d2
MatiasMtz <4442@holbertonschool.com>
2022-02-21 11:53:24 -0800

    function that sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - function that prints the sum of the two diagonals.
+ * @a: Pointer.
+ * @size: size of the matrix.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
