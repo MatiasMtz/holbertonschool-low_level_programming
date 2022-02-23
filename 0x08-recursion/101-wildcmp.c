936415bd4100e4eecdbf26cce1a1fef809ddf210
MatiasMtz <4442@holbertonschool.com>
2022-02-23 10:54:47 -0800

    compares two strings and returns 1 or 0

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings and returns 1 or 0.
+ * @s1: string to compare.
+ * @s2: string to compare.
+ * Return: length of the string.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
