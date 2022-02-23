87c8a32ce066b6db2f6a4bd326edb36abd7016bb
MatiasMtz <4442@holbertonschool.com>
2022-02-23 11:18:54 -0800

    compares two strings and returns 1 or 0

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,9 +3,23 @@
  * wildcmp - compares two strings and returns 1 or 0.
  * @s1: string to compare.
  * @s2: string to compare.
- * Return: length of the string.
+ * Return: 1 if are the same, 0 if not.
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (0);
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(*s1, *s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp (s1 + 1, s2 + 2));
+	}
+	return (-1);
 }
