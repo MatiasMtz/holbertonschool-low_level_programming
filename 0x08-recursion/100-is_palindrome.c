cabf4c2e5ca03b241426356b89aaaa5fb32b1058
MatiasMtz <4442@holbertonschool.com>
2022-02-23 09:33:18 -0800

    returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: pointer to s.
+ * Return: length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (s[length] == '\0')
+	{
+		return (length);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * palindrome - palindrome calculator.
+ * @word: string to check.
+ * @beginning: beginning of the word.
+ * @end: end of the word.
+ * Return: is palindrome?.
+ */
+int palindrome(char word[], int beginning, int end)
+{
+	if (beginning == end)
+		return (1);
+	if (word[beginning] != word[end])
+		return (0);
+	if (beginning < end + 1)
+		return (palindrome(word, beginning + 1, end - 1));
+	return (1);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: ponter to the string.
+ * Return: 1 if true, 0 if false.
+ */
+int is_palindrome(char *s)
+{
+	int l = _strlen_recursion(s) - 1;
+
+	return (palindrome(s, 0, l));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,3 +7,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
 int square_root(int a, int b);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
