748a718664e20d616c573ca1487efec3099e6515
MatiasMtz <4442@holbertonschool.com>
2022-02-25 07:40:32 -0800

    adds positive numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,22 +16,21 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (1);
 	}
-	else
-		for (count = 1; count < argc; count++)
+	for (count = 1; count < argc; count++)
+	{
+		for (letter = 0; letter < (int)strlen(argv[count]); letter++)
 		{
-			for (letter = 0; letter < (int)strlen(argv[count]);
-			     letter++)
+			if (!((argv[count][letter] >= '0') &&
+			      (argv[count][letter] <= '9')))
 			{
-				if (!((argv[count][letter] >= '0') &&
-				      (argv[count][letter] <= '9')))
-				{
-					printf("Error\n");
-					return (1);
-				}
+				printf("Error\n");
+				return (1);
 			}
-		number += (atoi(argv[count]));
 		}
+		number += (atoi(argv[count]));
+	}
 	printf("%d\n", number);
 	return (0);
 }
