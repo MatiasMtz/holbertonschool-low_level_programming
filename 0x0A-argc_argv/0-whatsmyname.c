2fc8546a2c63b83d24587a2b95f1e9ba39863d93
MatiasMtz <4442@holbertonschool.com>
2022-02-25 04:38:28 -0800

    prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: argument count.
+ * @argv: argument vector.
+ */
+void main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
