c2072356cbf9d03ed2ec2382845ecf13e0d74449
MatiasMtz <4442@holbertonschool.com>
2022-02-25 07:27:55 -0800

    multiplies two numbers.

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,8 +4,9 @@
  * main - program that prints its name, followed by a new line.
  * @argc: argument count.
  * @argv: argument vector.
+ * Return: 0 if success
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int number;
 
@@ -16,4 +17,5 @@ void main(int argc, char *argv[])
 	}
 	else
 	printf("Error\n");
+	return (0);
 }
