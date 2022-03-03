0a9ba8510b6475d8ac5523c3f9996cd658ab6465
MatiasMtz <4442@holbertonschool.com>
2022-03-03 11:13:17 -0800

    concatenates two strings.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,10 +6,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *a = malloc(b);
-		if (a == NULL)
-		{
-			exit(98);
-		}
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	{
+		exit(98);
+	}
 	return (a);
 }
