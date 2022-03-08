78b75f2b760b582e3d06ac458f92b5f315cca9c4
MatiasMtz <4442@holbertonschool.com>
2022-03-08 15:24:53 +0000

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,9 @@
 #define DOG_H
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
