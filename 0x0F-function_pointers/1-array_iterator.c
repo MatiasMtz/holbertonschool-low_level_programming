c4eb9140c7acca724afb715dc8b7603923b974b2
MatiasMtz <4442@holbertonschool.com>
2022-03-09 15:22:48 +0000

    executes a function that makes an action in each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
+	if (array == NULL)
+		return;
+	if (action == NULL)
+		return;
 	while (i < size)
 	{
 		action(array[i]);
