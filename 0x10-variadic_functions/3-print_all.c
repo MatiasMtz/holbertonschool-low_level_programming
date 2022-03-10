6c5a03dcaf7faa1c5cfbe84121720d45d0bda53b
MatiasMtz <4442@holbertonschool.com>
2022-03-10 20:28:54 +0000

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,9 +1,10 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * print_string - Pritns strings
  * @args: Arguments
+ * 
   */
 void print_string(char *space, va_list args)
 {
