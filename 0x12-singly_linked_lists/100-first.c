ddf6390419c77ee528562f21994bc60382eb6c76
MatiasMtz <4442@holbertonschool.com>
2022-03-17 16:57:41 +0000

    function that prints before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,5 +4,6 @@
  */
 void beforemain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf(
+\"You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
