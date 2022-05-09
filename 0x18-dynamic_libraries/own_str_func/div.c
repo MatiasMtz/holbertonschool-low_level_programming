2742f9523fc7a6a21d3629daa509968ba19c6441
MatiasMtz <4442@holbertonschool.com>
2022-05-09 16:38:57 +0000

    dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/own_str_func/div.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * div - division.
+ * @a: first parameter.
+ * @b: second parameter.
+ * Return: the result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/own_str_func/mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply a to b.
+ * @a: first parameter.
+ * @b: second parameter.
+ * Return: the result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
