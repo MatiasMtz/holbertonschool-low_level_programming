ee7da45e1bbc6085bbb5a002ba63d3d3d00f020e
MatiasMtz <4442@holbertonschool.com>
2022-05-10 03:02:23 +0000

    refresh

--- 0x18-dynamic_libraries/injection.c
+++ 0x18-dynamic_libraries/injection.c
@@ -4,7 +4,7 @@ int printf(const char *format, ...)
 	return (0);
 }
 
-int puts (const char *str)
+int puts(const char *str)
 {
 	return (write(STDOUT_FILENO, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57));
 }
