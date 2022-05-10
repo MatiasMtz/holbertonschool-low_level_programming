a74615637890b8ba09aaec8fc62c9007191395bb
MatiasMtz <4442@holbertonschool.com>
2022-05-10 02:50:27 +0000

    refresh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/code_injection https://github.com/MatiasMtz/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/code_injection.so?raw=true
-export LD_PRELOAD="$PWD/../code_injection.so"
+wget -O /tmp/code_injection.so https://github.com/MatiasMtz/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/code_injection.so
+export LD_PRELOAD=/tmp/code_injection.so
